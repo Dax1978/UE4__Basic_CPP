2999ccfcad82de172e834822430864b4b93ad1bf
Dax <John-Xenos@yandex.ru>
2022-04-27 18:47:45 +0300

    Практическое задание 8

---
+++ Lesson_08/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#include "variable.h"
+#include "TicTacToeLib.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    tictactoe();
+
+    std::cout << "Спасибо за Вашу игру!\n";
+    return 0;
+}
